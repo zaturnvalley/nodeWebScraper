cf9e7f57a733e086fb4c6b15b916dcb76141964a
zaturnvalley <zacharycmason@gmail.com>
2018-03-12 15:06:02 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Web scraping in Node
+const rp = require('request-promise');
+const cherio = require('cheerio');
+const Table = require('cli-table');
+
+let users = [];
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Basic Web Scraping with NodeJS
+
+## Uses:
+* request-promise
+* cherio
+* cli-table
+
+## Getting started:
+* Clone
+* Run 'npm install'
