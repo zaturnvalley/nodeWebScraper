ce0478ec5c6b1362c466a86098ca3b43b534ed5d
zaturnvalley <zacharycmason@gmail.com>
2018-03-12 15:18:22 -0700

    retrieving data from free code camp, request callback

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const Table = require('cli-table');
 
 let users = [];
 
+const options = {
+  url: `https://forum.freecodecamp.org/directory_items?period=weekly&order=likes_received&_=1520892991855`,
+  json: true
+}
+
+rp(options)
+  .then((data) => {
+    let userData = [];
+
+    for (let user of data.directory_items) {
+      userData.push({name: user.user.username,likes_received: user.likes_received});
+    }
+
+  })
